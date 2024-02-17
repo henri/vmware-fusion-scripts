8a5261af6ec623f85da2384aa2489b24db863f30
henri <henri.shustak@gmail.com>
2024-02-18 00:31:07 +1300

    added improved notes regarding setup example copy script

--- copy_vms_to_backup_directory_example.bash
+++ copy_vms_to_backup_directory_example.bash
@@ -9,6 +9,10 @@
 
 # This is a basic example script which simply rsync's the (probably) suspended VM's from one directory to another.
 # you will need to edit this script to make it work for your setup and installation enviroment.
+# This example script assumes that this repository sits within a directory of vm's and
+# this folder has a directory called backups which will be used to copy the suspended vms into
+# In addition, this script excludes a vm called test from the bcakup. It is suggested these options
+# are altered to meet your needs, this is just an example / template file.
 
 ##
 ## Danger : This script is running rsync with the --delete command. You must be very carful not to delete important files. 
